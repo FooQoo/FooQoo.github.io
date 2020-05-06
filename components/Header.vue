c071161deda89cf1e919b884d2b2845613798d41
FooQoo <horseshoes.buzz.17@gmail.com>
2020-05-06 19:19:27 +0900

    タイトルをh1にする

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,8 @@
 <template>
   <header>
     <nav class="navbar py-3 d-flex navbar-light border-0 container">
-      <router-link
-        class="navbar-brand font-serif font-weight-bold"
-        to="/"
-      >{{$site.title}}
+      <router-link class="navbar-brand" to="/">
+        <h1 class="font-serif font-weight-bold">{{ $site.title }}</h1>
       </router-link>
 
       <SearchBox />
@@ -12,14 +10,9 @@
 
     <nav class="navbar navbar-light border-0 container x-overflow-scroll">
       <ul class="navbar-nav mx-auto d-flex flex-row">
-        <li
-          class="text-uppercase mx-2"
-        >
-          <router-link
-            class="nav-link text-nowrap "
-            to="/"
-          >
-          HOME
+        <li class="text-uppercase mx-2">
+          <router-link class="nav-link text-nowrap " to="/">
+            HOME
           </router-link>
         </li>
         <li
@@ -27,13 +20,9 @@
           v-for="tag in $tag.list"
           :key="tag.name"
         >
-          <router-link
-            class="nav-link text-nowrap "
-            :to="tag.path"
-          >
-            {{tag.name}}
+          <router-link class="nav-link text-nowrap " :to="tag.path">
+            {{ tag.name }}
           </router-link>
-
         </li>
       </ul>
     </nav>
@@ -41,11 +30,11 @@
 </template>
 
 <script>
-import SearchBox from './SearchBox'
+import SearchBox from "./SearchBox";
 
 export default {
   components: { SearchBox },
-}
+};
 </script>
 
 <style lang="stylus" scoped>
