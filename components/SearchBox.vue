6f4292b1ccbe4959aaf0602ef82a9ee9a7d4c5ab
FooQoo <horseshoes.buzz.17@gmail.com>
2020-05-06 20:47:27 +0900

    fix input size

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -4,7 +4,7 @@
       @input="query = $event.target.value"
       aria-label="Search"
       :value="query"
-      :class="{ 'focused': focused }"
+      :class="{ focused: focused }"
       autocomplete="off"
       spellcheck="false"
       @focus="focused = true"
@@ -12,7 +12,7 @@
       @keyup.enter="go(focusIndex)"
       @keyup.up="onUp"
       @keyup.down="onDown"
-    >
+    />
     <ul
       class="suggestions"
       v-if="showSuggestions"
@@ -27,15 +27,9 @@
         @mouseenter="focus(i)"
         :key="i"
       >
-        <a
-          :href="s.path"
-          @click.prevent
-        >
+        <a :href="s.path" @click.prevent>
           <span class="page-title">{{ s.title || s.path }}</span>
-          <span
-            v-if="s.header"
-            class="header"
-          >&gt; {{ s.header.title }}</span>
+          <span v-if="s.header" class="header">&gt; {{ s.header.title }}</span>
         </a>
       </li>
     </ul>
@@ -45,137 +39,139 @@
 <script>
 /* global SEARCH_MAX_SUGGESTIONS, SEARCH_PATHS */
 export default {
-  data () {
+  data() {
     return {
-      query: '',
+      query: "",
       focused: true,
-      focusIndex: 0
-    }
+      focusIndex: 0,
+    };
   },
 
   computed: {
-    showSuggestions () {
-      return (
-        this.focused
-        && this.suggestions
-        && this.suggestions.length
-      )
+    showSuggestions() {
+      return this.focused && this.suggestions && this.suggestions.length;
     },
 
-    suggestions () {
-      const query = this.query.trim().toLowerCase()
+    suggestions() {
+      const query = this.query.trim().toLowerCase();
       if (!query) {
-        return
+        return;
       }
 
-      const { pages } = this.$site
-      const max = SEARCH_MAX_SUGGESTIONS
-      const localePath = this.$localePath
-      const matches = item => (
-        item.title
-        && item.title.toLowerCase().indexOf(query) > -1
-      )
-      const res = []
+      const { pages } = this.$site;
+      const max = SEARCH_MAX_SUGGESTIONS;
+      const localePath = this.$localePath;
+      const matches = (item) =>
+        item.title && item.title.toLowerCase().indexOf(query) > -1;
+      const res = [];
       for (let i = 0; i < pages.length; i++) {
-        if (res.length >= max) break
-        const p = pages[i]
+        if (res.length >= max) break;
+        const p = pages[i];
         // filter out results that do not match current locale
         if (this.getPageLocalePath(p) !== localePath) {
-          continue
+          continue;
         }
 
         // filter out results that do not match searchable paths
         if (!this.isSearchable(p)) {
-          continue
+          continue;
         }
 
         if (matches(p)) {
-          res.push(p)
+          res.push(p);
         } else if (p.headers) {
           for (let j = 0; j < p.headers.length; j++) {
-            if (res.length >= max) break
-            const h = p.headers[j]
+            if (res.length >= max) break;
+            const h = p.headers[j];
             if (matches(h)) {
-              res.push(Object.assign({}, p, {
-                path: p.path + '#' + h.slug,
-                header: h
-              }))
+              res.push(
+                Object.assign({}, p, {
+                  path: p.path + "#" + h.slug,
+                  header: h,
+                })
+              );
             }
           }
         }
       }
-      return res
+      return res;
     },
 
     // make suggestions align right when there are not enough items
-    alignRight () {
-      const navCount = (this.$site.themeConfig.nav || []).length
-      const repo = this.$site.repo ? 1 : 0
-      return navCount + repo <= 2
-    }
+    alignRight() {
+      const navCount = (this.$site.themeConfig.nav || []).length;
+      const repo = this.$site.repo ? 1 : 0;
+      return navCount + repo <= 2;
+    },
   },
 
   methods: {
-    getPageLocalePath (page) {
+    getPageLocalePath(page) {
       for (const localePath in this.$site.locales || {}) {
-        if (localePath !== '/' && page.path.indexOf(localePath) === 0) {
-          return localePath
+        if (localePath !== "/" && page.path.indexOf(localePath) === 0) {
+          return localePath;
         }
       }
-      return '/'
+      return "/";
     },
 
-    isSearchable (page) {
-      let searchPaths = SEARCH_PATHS
+    isSearchable(page) {
+      let searchPaths = SEARCH_PATHS;
 
       // all paths searchables
-      if (searchPaths === null) { return true }
+      if (searchPaths === null) {
+        return true;
+      }
 
-      searchPaths = Array.isArray(searchPaths) ? searchPaths : new Array(searchPaths)
+      searchPaths = Array.isArray(searchPaths)
+        ? searchPaths
+        : new Array(searchPaths);
 
-      return searchPaths.filter(path => {
-        return page.path.match(path)
-      }).length > 0
+      return (
+        searchPaths.filter((path) => {
+          return page.path.match(path);
+        }).length > 0
+      );
     },
 
-    onUp () {
+    onUp() {
       if (this.showSuggestions) {
         if (this.focusIndex > 0) {
-          this.focusIndex--
+          this.focusIndex--;
         } else {
-          this.focusIndex = this.suggestions.length - 1
+          this.focusIndex = this.suggestions.length - 1;
         }
       }
     },
 
-    onDown () {
+    onDown() {
       if (this.showSuggestions) {
         if (this.focusIndex < this.suggestions.length - 1) {
-          this.focusIndex++
+          this.focusIndex++;
         } else {
-          this.focusIndex = 0
+          this.focusIndex = 0;
         }
       }
     },
 
-    go (i) {
+    go(i) {
       if (!this.showSuggestions) {
-        return
+        return;
       }
-      this.$router.push(this.suggestions[i].path)
-      this.query = ''
-      this.focusIndex = 0
+      this.$router.push(this.suggestions[i].path);
+      this.query = "";
+      this.focusIndex = 0;
     },
 
-    focus (i) {
-      this.focusIndex = i
+    focus(i) {
+      this.focusIndex = i;
     },
 
-    unfocus () {
-      this.focusIndex = -1
-    }
-  }
-}
+    unfocus() {
+      this.focusIndex = -1;
+    },
+  },
+};
 </script>
 
 <style lang="stylus">
@@ -188,7 +184,7 @@ export default {
   input {
     cursor: text;
     width: 0rem;
-    height: 2rem;
+    height: 3rem;
     color: lighten($textColor, 25%);
     display: inline-block;
     border: none;
